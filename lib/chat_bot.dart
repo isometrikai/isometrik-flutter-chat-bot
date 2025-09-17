cb0c3e2f86ebfdd682fe5e8e9ce240b85b701f67
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-09-17 20:34:25 +0530

    - add logs

--- lib/chat_bot.dart
+++ lib/chat_bot.dart
@@ -63,7 +63,7 @@ class ChatBot {
     );
   }
 
-  static void isCartUpdate(dynamic cartData) {
+   void isCartUpdate() {
     // print('isCartUpdate: $cartData');
     // Trigger the cart update callback to notify both ChatScreen and RestaurantScreen
     // OrderService().triggerCartUpdate(true);
