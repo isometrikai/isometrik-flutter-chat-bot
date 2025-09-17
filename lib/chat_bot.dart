d3eff7ae24e56703f886c0456855629683670a35
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-09-17 16:45:42 +0530

    - removed carddata print

--- lib/chat_bot.dart
+++ lib/chat_bot.dart
@@ -64,7 +64,7 @@ class ChatBot {
   }
 
   static void isCartUpdate(dynamic cartData) {
-    print('isCartUpdate: $cartData');
+    // print('isCartUpdate: $cartData');
     // Trigger the cart update callback to notify both ChatScreen and RestaurantScreen
     OrderService().triggerCartUpdate(true);
   }
