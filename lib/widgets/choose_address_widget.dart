371c94e31c5653a372a12380a7d3958e76e52398
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-10-08 12:31:14 +0530

    - Solved issue

--- lib/widgets/choose_address_widget.dart
+++ lib/widgets/choose_address_widget.dart
@@ -93,7 +93,7 @@ class _ChooseAddressWidgetState extends State<ChooseAddressWidget> {
           widget.onAddressSelected?.call(addressOption);
           
           // Automatically send the selected address as a message
-          final message = "Use this address:-\n${addressOption.name}: ${addressOption.address}";
+          final message = "Use this address\n${addressOption.name}: ${addressOption.address}";
           widget.onSendMessage?.call(message);
         }
       },
