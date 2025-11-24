84d7a119ddbba04118349931ef9cdc1f4e0e18f4
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-11-24 16:55:48 +0530

    - Solved issues

--- lib/services/callback_manage.dart
+++ lib/services/callback_manage.dart
@@ -17,7 +17,7 @@ class OrderService {
   Function(String)? onStripePayment; // Add stripe payment callback
   Function(String)? onAddressSummary; // Add order summary callback
   Function(String)? onSendMessage; // Add send message callback // CHANGE CALLBACK
-  Function(String)? onSelectSchedule; // Add select schedule callback
+  Function(Map<String, dynamic>)? onSelectSchedule; // Add select schedule callback
   Function(String)? onSelectStaff; // Add select staff callback
 
   void setProductCallback(Function(Map<String, dynamic>) callback) {
@@ -71,7 +71,7 @@ class OrderService {
     onAddressSummary = callback;
   }
 
-  void setSelectScheduleCallback(Function(String) callback) {
+  void setSelectScheduleCallback(Function(Map<String, dynamic>) callback) {
     print('setSelectScheduleCallback: $callback');
     onSelectSchedule = callback;
   }
@@ -102,7 +102,7 @@ class OrderService {
 
   void triggerScheduledLaterScreenOpen(Map<String, dynamic> obj) {
     print('triggerScheduledLaterScreenOpen: $obj');
-    onScheduledLaterScreenOpen?.call(obj);
+    onSelectSchedule?.call(obj);
   }
 
   void triggerSelectStaffScreenOpen(Map<String, dynamic> obj) {
@@ -143,7 +143,7 @@ class OrderService {
     onSendMessage?.call(message);
   }
 
-  void triggerSelectSchedule(String schedule) {
+  void triggerSelectSchedule(Map<String, dynamic> schedule) {
     print('triggerSelectSchedule: $schedule');
     onSelectSchedule?.call(schedule);
   }
