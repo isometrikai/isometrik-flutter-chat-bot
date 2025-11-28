46a11216d430245e67293315dd6d9f08ae478b3c
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-11-28 11:16:39 +0530

    - Changed Image

--- lib/view/chat_screen.dart
+++ lib/view/chat_screen.dart
@@ -958,7 +958,7 @@ class _ChatScreenState extends State<ChatScreen> {
                         child: Padding(
                           padding: const EdgeInsets.all(12),
                           child: SvgPicture.asset(
-                            AssetPath.get('images/ic_mainImg.svg'),
+                            AssetPath.get('images/ic_mainImg_R.svg'),
                             fit: BoxFit.contain,
                           ),
                         ),
@@ -2273,7 +2273,7 @@ class _ChatScreenBody extends StatelessWidget {
                         child: Padding(
                           padding: const EdgeInsets.all(12),
                           child: SvgPicture.asset(
-                            AssetPath.get('images/ic_mainImg.svg'),
+                            AssetPath.get('images/ic_mainImg_R.svg'),
                             fit: BoxFit.contain,
                           ),
                         ),
