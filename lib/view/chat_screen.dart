78c1ec9d426147912c8fe69e7b576d99962212b0
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2025-12-10 12:32:38 +0530

    - solved issue

--- lib/view/chat_screen.dart
+++ lib/view/chat_screen.dart
@@ -121,15 +121,14 @@ class _ChatScreenState extends State<ChatScreen> {
     OrderService().setSelectScheduleCallback((Map<String, dynamic> schedule) {
       if (mounted) {
         print('ChatScreen: Select schedule received - $schedule');
-        _sendMessage('I have selected a schedule.\n${schedule['dateTimeStr']}', schedule['serviceRequestedTime']);
+        _sendMessage('I have selected a schedule.\n${schedule['dateTimeStr']}', schedule['scheduleLaterStaffId'], schedule['serviceRequestedTime']);
       }
     });
 
     OrderService().setSelectStaffCallback((Map<String, dynamic> staff) {
       if (mounted) {
         print('ChatScreen: Select staff received - $staff');
-        // _sendMessage('I have selected a staff.\n$staff');
-        _sendMessage('I have selected a staff.\n${staff['dateTimeStr']}', staff['serviceRequestedTime']);
+        _sendMessage('I have selected a staff.\n$staff', staff['scheduleLaterStaffId'], staff['serviceRequestedTime']);
       }
     });
   }
