3d68cefdbaa5c28266165729b90409e4dea5e7b7
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2026-06-22 15:48:12 +0530

    - debug issue log

--- lib/widgets/chat_screen_body.dart
+++ lib/widgets/chat_screen_body.dart
@@ -573,21 +573,45 @@ class ChatScreenBody extends StatelessWidget {
                                     ),
                                   );
                                   }else {
-                                    Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder:
-                                          (context) => BlocProvider(
-                                            create: (context) => CartBloc(),
-                                            child: CartScreen(
-                                              needToEndThisChat: needToEndThisChat,
-                                              onCheckout: (message, storeCategoryId) {
-                                                onSendMessage(message, null, null, storeCategoryId);
-                                              },
-                                            ),
-                                          ),
-                                    ),
-                                  );
+                                    final userId = ChatApiServices.instance.userId ?? '';
+                          final orderId = 'XCBID-0000000094';
+                          final data = <String, dynamic>{
+                                "metadata": {
+                                    "trigger": "order",
+                                    "orderId": orderId,
+                                    "userType": "user",
+                                    "paymentAction": 2,
+                                    "userId": userId
+                                },
+                                "userId": userId,
+                                "amount": '1350.13',
+                                "currency": 'AED',
+                                "userType": "user",
+                                "capture": false,
+                                "paymentAction": 2,
+                                "orderId": orderId
+                            };
+                            if (gotStripePaymentCallback == false) {
+                               Timer(Duration(seconds: 1), () {
+                                print("Timer completed");
+                                OrderService().triggerStripePlaceOrderScreenOpen(data);
+                              });
+                            }
+                                  //   Navigator.push(
+                                  //   context,
+                                  //   MaterialPageRoute(
+                                  //     builder:
+                                  //         (context) => BlocProvider(
+                                  //           create: (context) => CartBloc(),
+                                  //           child: CartScreen(
+                                  //             needToEndThisChat: needToEndThisChat,
+                                  //             onCheckout: (message, storeCategoryId) {
+                                  //               onSendMessage(message, null, null, storeCategoryId);
+                                  //             },
+                                  //           ),
+                                  //         ),
+                                  //   ),
+                                  // );
                                   }
                                 },
                       ),
