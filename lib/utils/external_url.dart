afb547cada43770ed224099d5a31714b3787e611
ChetanAppScrip <103981861+ChetanAppScrip@users.noreply.github.com>
2026-08-27 10:08:48 +0530

    - Solved android subscription issue

--- lib/utils/external_url.dart
+++ lib/utils/external_url.dart
@@ -1,33 +1,70 @@
 import 'dart:io';
 
+import 'package:chat_bot/services/in_app_purchase/iap_product_ids.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// Opens [url] in the device's default browser / external app.
-Future<bool> openUrl(String url) async {
+Future<bool> openUrl(String url, {LaunchMode? mode}) async {
   final uri = Uri.tryParse(url);
   if (uri == null) {
     print('Could not parse url: $url');
     return false;
   }
+  return _launchUri(uri, mode ?? LaunchMode.externalApplication);
+}
+
+Future<bool> _launchUri(Uri uri, LaunchMode mode) async {
   try {
-    final launched = await launchUrl(
-      uri,
-      mode: LaunchMode.externalApplication,
-    );
+    final launched = await launchUrl(uri, mode: mode);
     if (!launched) {
-      print('Could not launch $url');
+      print('Could not launch $uri (mode=$mode)');
     }
     return launched;
   } catch (e) {
-    print('Could not launch $url: $e');
+    print('Could not launch $uri: $e');
     return false;
   }
 }
 
 /// Opens the platform subscription management page (App Store / Play Store).
-Future<bool> openStoreSubscriptions() {
-  final url = Platform.isIOS
-      ? 'https://apps.apple.com/account/subscriptions'
-      : 'https://play.google.com/store/account/subscriptions';
-  return openUrl(url);
+Future<bool> openStoreSubscriptions() async {
+  if (Platform.isIOS) {
+    return openUrl('https://apps.apple.com/account/subscriptions');
+  }
+  return _openAndroidStoreSubscriptions();
+}
+
+Future<bool> _openAndroidStoreSubscriptions() async {
+  final packageInfo = await PackageInfo.fromPlatform();
+  final packageName = packageInfo.packageName;
+  final productId = IapProductIds.androidSubscription;
+
+  final subscriptionUri = Uri.https(
+    'play.google.com',
+    '/store/account/subscriptions',
+    {'sku': productId, 'package': packageName},
+  );
+  final allSubscriptionsUri = Uri.https(
+    'play.google.com',
+    '/store/account/subscriptions',
+  );
+  final playStoreIntentUri = Uri.parse(
+    'intent://play.google.com/store/account/subscriptions'
+    '?sku=$productId&package=$packageName'
+    '#Intent;scheme=https;package=com.android.vending;end',
+  );
+
+  // Prefer Play Store app over browser (Android 11+ needs manifest queries).
+  for (final uri in [
+    subscriptionUri,
+    playStoreIntentUri,
+    allSubscriptionsUri,
+  ]) {
+    if (await _launchUri(uri, LaunchMode.externalNonBrowserApplication)) {
+      return true;
+    }
+  }
+
+  return _launchUri(subscriptionUri, LaunchMode.externalApplication);
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import flutter_tts
 import in_app_purchase_storekit
+import package_info_plus
 import shared_preferences_foundation
 import speech_to_text
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
